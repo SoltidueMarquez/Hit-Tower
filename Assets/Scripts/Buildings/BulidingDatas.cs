1116796a65322cc4ee7f39c7f811c614d0e9b792
SoltidueMarquez <90582307+SoltidueMarquez@users.noreply.github.com>
2025-11-02 12:59:21 +0800

    建筑物

---
+++ Assets/Scripts/Buildings/BulidingDatas.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Buildings
+{
+    [CreateAssetMenu(fileName = "Building", menuName = "Buildings/BuildingData")]
+    public class BulidingDatas : ScriptableObject
+    {
+        public List<BuildingData> buildingDataList = new List<BuildingData>();
+    }
+
+    [Serializable]
+    public class BuildingData
+    {
+        [LabelText("唯一标识符")] public string buildingName;
+        [LabelText("等级数值信息")] public List<BuildingLevelData> levelData;
+        public int MaxLv => levelData.Count;
+    }
+
+    [Serializable]
+    public class BuildingLevelData
+    {
+        [LabelText("攻击力")] public float attack;
+        [LabelText("攻击范围")] public float attackRange;
+        [LabelText("攻击间隔")] public float attackInterval;
+        [LabelText("单体/群体攻击")] public float ifMulti;
+        [EnableIf("ifMulti"),LabelText("同时攻击的敌人个数")] public int attackNum;
+        [Header("暂时用不上")] public float maxHealth;
+    }
+}
